530c3cd52c4b00d5072a8f7b971b25a9840271b5
Huy Nguyen <xanhy8k99@gmail.com>
2024-02-04 14:19:20 +0100

    feat: functional KDModel

--- model.py
+++ model.py
@@ -35,8 +35,8 @@ class BaseModel(L.LightningModule, ABC):
         return {'loss': self.ce_loss(logits, labels), 'metrics': self.accuracy(logits, labels)}
     
     def on_train_batch_end(self, outputs, batch, batch_idx) -> None:
-        self.log('step_loss', outputs['loss'], prog_bar=True, sync_dist=False)
-        self.log('step_metrics', outputs['metrics'], sync_dist=False)
+        self.log('step_loss', outputs['loss'], prog_bar=True)
+        self.log('step_metrics', outputs['metrics'])
         
         if not self.train_step_outputs:
             self.train_step_outputs = {
@@ -63,8 +63,8 @@ class BaseModel(L.LightningModule, ABC):
         return {'val_loss': self.ce_loss(logits, labels), 'metrics': self.accuracy(logits, labels)}
     
     def on_validation_batch_end(self, outputs, batch, batch_idx, dataloader_idx = 0) -> None:
-        self.log('step_val_loss', outputs['val_loss'], prog_bar=True, sync_dist=False)
-        self.log('step_val_metrics', outputs['metrics'], sync_dist=False)
+        self.log('step_val_loss', outputs['val_loss'], prog_bar=True)
+        self.log('step_val_metrics', outputs['metrics'])
         
         if not self.valid_step_outputs:
             self.valid_step_outputs = {
@@ -81,8 +81,8 @@ class BaseModel(L.LightningModule, ABC):
         mean_loss_value = torch.tensor(self.valid_step_outputs['step_val_loss']).mean()
         mean_metrics_value = torch.tensor(self.valid_step_outputs['step_val_metrics']).mean()
         
-        self.log('val_loss', mean_loss_value, sync_dist=False)
-        self.log('val_Accuracy', mean_metrics_value, sync_dist=False)
+        self.log('val_loss', mean_loss_value)
+        self.log('val_Accuracy', mean_metrics_value)
         
         tb_logger.add_scalar('Loss/loss', mean_loss_value, self.current_epoch)
         tb_logger.add_scalar('Metrics/accuracy', mean_metrics_value, self.current_epoch)
@@ -159,15 +159,23 @@ class SmallModel(BaseModel):
             nn.Linear(320, num_classes)
         )
         
+        self.model = nn.Sequential(
+            self.features,
+            nn.Flatten(1),
+            self.classifier
+        )
+        
         self.train_step_outputs = {}
         self.valid_step_outputs = {}
 
     def forward(self, x):
-        x = self.features(x)
-        flattened_conv_output = torch.flatten(x, 1)
-        x = self.classifier(flattened_conv_output)
-        # flattened_conv_output_after_pooling = torch.nn.functional.avg_pool1d(flattened_conv_output, 2)
-        return x, flattened_conv_output
+        # x = self.features(x)
+        # flattened_conv_output = torch.flatten(x, 1)
+        # x = self.classifier(flattened_conv_output)
+        # # flattened_conv_output_after_pooling = torch.nn.functional.avg_pool1d(flattened_conv_output, 2)
+        logits = self.model(x)
+        flattened_conv_output = torch.flatten(self.features(x), 1)
+        return logits, flattened_conv_output
     
     def training_step(self, batch, batch_idx):
         images, labels = batch
@@ -190,15 +198,13 @@ class SmallModel(BaseModel):
         self.valid_step_outputs.clear()
 
 
-class KDModel(L.LightningModule):
+class KDModel(BaseModel):
     def __init__(self, opt: Namespace):
-        super(KDModel, self).__init__()
+        super().__init__(opt)
         self.opt = opt
         
         teacher_ckpt_path = Path('weights_storage/version_1/epoch=49-step=39100.ckpt')
-        teacher_ckpt = torch.load(teacher_ckpt_path, device=self.device)
-        self.teacher = BigModel(opt)
-        self.teacher.load_state_dict(teacher_ckpt['state_dict'])
+        self.teacher = BigModel.load_from_checkpoint(teacher_ckpt_path, opt=opt, map_location=self.device)
         self.teacher.eval()
         
         self.student = SmallModel(opt)
@@ -214,8 +220,8 @@ class KDModel(L.LightningModule):
 
     def forward(self, x):
         with torch.no_grad():
-            out_teacher = self.teacher.features(x)
-            out_teacher = self.teacher.avgpool(out_teacher)
+            out_teacher = self.teacher.model.features(x)
+            out_teacher = self.teacher.model.avgpool(out_teacher).squeeze()
             
         out_student = self.student(x)
         return {'teacher': out_teacher, 'student': out_student}
@@ -226,7 +232,7 @@ class KDModel(L.LightningModule):
         
         assert outputs['teacher'].shape == outputs['student'][1].shape, 'teacher output and student flattened features have different shapes'
         
-        emb_loss = self.cos_emb_loss(outputs['teacher'], outputs['student'][1])
+        emb_loss = self.cos_emb_loss(outputs['teacher'], outputs['student'][1], target=torch.ones(images.size(0), device='mps'))
         logits_loss = self.ce_loss(outputs['student'][0], labels)
         
         return {
@@ -237,26 +243,25 @@ class KDModel(L.LightningModule):
         }
     
     def on_train_batch_end(self, outputs, batch, batch_idx) -> None:
-        self.log('step_loss', outputs['loss'], prog_bar=True, sync_dist=False)
-        self.log('step_metrics', outputs['metrics'], sync_dist=False)
+        log_dict = {'step_loss': outputs['loss']}
+        log_dict.update({'step_' + k: v for k, v in outputs.items() if k != 'loss'})
+        self.log_dict(log_dict)
         
         if not self.train_step_outputs:
-            self.train_step_outputs = {
-                'step_loss': [outputs['loss']],
-                'step_metrics': [outputs['metrics']]
-            }
+            self.train_step_outputs = {k: [v] for k, v in log_dict.items()}
         else:
-            self.train_step_outputs['step_loss'].append(outputs['loss'])
-            self.train_step_outputs['step_metrics'].append(outputs['metrics'])
+            for k in log_dict.keys():
+                self.train_step_outputs[k].append(log_dict[k])
     
     def on_train_epoch_end(self):
         tb_logger = self.loggers[0].experiment
         
-        mean_loss_value = torch.tensor(self.train_step_outputs['step_loss']).mean()
         mean_metrics_value = torch.tensor(self.train_step_outputs['step_metrics']).mean()
-        
-        tb_logger.add_scalar('Loss/loss', mean_loss_value, self.current_epoch)
         tb_logger.add_scalar('Metrics/accuracy', mean_metrics_value, self.current_epoch)
+        
+        mean_losses_values = {k: torch.tensor(v).mean() for k, v in self.train_step_outputs.items() if 'loss' in k}
+        for k, v in mean_losses_values.items():
+            tb_logger.add_scalar('Loss/' + k.replace('step_', ''), v, self.current_epoch)
         
         self.train_step_outputs.clear()
 
@@ -266,7 +271,7 @@ class KDModel(L.LightningModule):
         
         assert outputs['teacher'].shape == outputs['student'][1].shape, 'teacher output and student flattened features have different shapes'
         
-        emb_loss = self.cos_emb_loss(outputs['teacher'], outputs['student'][1])
+        emb_loss = self.cos_emb_loss(outputs['teacher'], outputs['student'][1], target=torch.ones(images.size(0), device='mps'))
         logits_loss = self.ce_loss(outputs['student'][0], labels)
         
         return {
@@ -277,50 +282,28 @@ class KDModel(L.LightningModule):
         }
     
     def on_validation_batch_end(self, outputs, batch, batch_idx, dataloader_idx = 0) -> None:
-        self.log('step_val_loss', outputs['val_loss'], prog_bar=True, sync_dist=False)
-        self.log('step_val_metrics', outputs['metrics'], sync_dist=False)
+        log_dict = {'step_val_loss': outputs['val_loss']}
+        log_dict.update({'step_val_' + k: v for k, v in outputs.items() if k != 'loss'})
+        self.log_dict(log_dict)
         
         if not self.valid_step_outputs:
-            self.valid_step_outputs = {
-                'step_val_loss': [outputs['val_loss']],
-                'step_val_metrics': [outputs['metrics']]
-            }
+            self.valid_step_outputs = {k: [v] for k, v in log_dict.items()}
         else:
-            self.valid_step_outputs['step_val_loss'].append(outputs['val_loss'])
-            self.valid_step_outputs['step_val_metrics'].append(outputs['metrics'])
+            for k in log_dict.keys():
+                self.valid_step_outputs[k].append(log_dict[k])
     
     def on_validation_epoch_end(self) -> None:
         tb_logger = self.loggers[1].experiment
         
-        mean_loss_value = torch.tensor(self.valid_step_outputs['step_val_loss']).mean()
-        mean_metrics_value = torch.tensor(self.valid_step_outputs['step_val_metrics']).mean()
+        mean_accuracy_value = torch.tensor(self.valid_step_outputs['step_val_metrics']).mean()
+        tb_logger.add_scalar('Metrics/accuracy', mean_accuracy_value, self.current_epoch)
         
-        self.log('val_loss', mean_loss_value, sync_dist=False)
-        self.log('val_Accuracy', mean_metrics_value, sync_dist=False)
+        mean_losses_values = {k: torch.tensor(v).mean() for k,v in self.valid_step_outputs.items() if 'loss' in k}
+        for k,v in mean_losses_values.items():
+            tb_logger.add_scalar('Loss/' + k.replace('step_val_', ''), v, self.current_epoch)
+            
+        self.log('val_loss', mean_losses_values['step_val_loss'])
+        self.log('val_Accuracy', mean_accuracy_value)
         
-        tb_logger.add_scalar('Loss/loss', mean_loss_value, self.current_epoch)
-        tb_logger.add_scalar('Metrics/accuracy', mean_metrics_value, self.current_epoch)
         
         self.valid_step_outputs.clear()
-
-    def configure_optimizers(self):
-        optimizer = Adam(params=self.parameters(), lr=self.lr)
-        optim_dict = {'optimizer': optimizer}
-        if self.opt.lr_plateau:
-            optim_dict['lr_scheduler'] = {
-                'scheduler':ReduceLROnPlateau(
-                    optimizer, 
-                    patience=self.opt.scheduler_patience, 
-                    verbose=True, 
-                    threshold=0.005,
-                    threshold_mode='abs'
-                    ),
-                'monitor': 'val_loss'
-            }
-        else:
-            lambda1 = lambda epoch: self.opt.rate_decay ** epoch
-            optim_dict['lr_scheduler'] = LambdaLR(optimizer, lr_lambda=lambda1, verbose=True)
-            
-        return optim_dict
-
-
